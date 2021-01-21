5165d38bbebe536b7a6daaae2298e1d60d9ce608
denl <denisee.larsson@gmail.com>
2021-01-21 10:54:22 +0100

    first shot react

---
+++ src/hello/HelloWorld01.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HelloWorld01() {
+
+    return(
+
+        //<h1>Hello world!</h1>
+
+        <h1 style= {{ backgroundColor: "yellow"}}>Hello world!</h1>
+    )
+}
---
+++ src/hello/HelloWorld02.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HelloWorld02() {
+
+    return(
+
+        <h1>Hei maaima</h1>
+    )
+}
